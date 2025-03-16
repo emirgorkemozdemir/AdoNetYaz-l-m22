7ad16fba6ac7b0060435698662de5c20b3c3062e
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2025-03-16 14:25:24 +0300

    Add project files.

---
+++ AdoNetYazılım22/Controllers/AracController.cs
@@ -0,0 +1,36 @@
+﻿using AdoNetYazılım22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoNetYazılım22.Controllers
+{
+    public class AracController : Controller
+    {
+        OtoServisDBEntities db = new OtoServisDBEntities();
+
+        [HttpGet]
+        public ActionResult AraclarıYukle()
+        {
+            List<araclar> arac_listesi = db.araclars.ToList();
+            return View(arac_listesi);
+        }
+
+
+        [HttpGet] // http get sayfa yüklendiginde çalışıyor.
+        public ActionResult AracEkle()
+        {
+            return View();
+        }
+
+        [HttpPost] // diger taraftan submit tuşuna basıldıgında post çalışır
+        public ActionResult AracEkle(araclar eklenen_arac)
+        {
+            db.araclars.Add(eklenen_arac);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ AdoNetYazılım22/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdoNetYazılım22.MvcApplication" Language="C#" %>
